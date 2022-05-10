551594c6433bdfce5e745c973346c567f65dce9b
ztes.sunny <xy@ztes.com>
2022-05-10 19:10:53 +0800

    button升级

--- packages/blocks/Button/packages/BButton.vue
+++ packages/blocks/Button/packages/BButton.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="b-button" @click="VueClick">
+  <div class="b-button" :class="[buttonSize,buttonDisplay,buttonShape]" :style="[buttonRadius]">
     <div class="b-button-loading" v-if="isLoading">
-      <div class="loading-css"></div>
-      <svg viewBox="0 0 50 50" class="loading-svg">
+      <!--      <div class="loading-css"></div>-->
+      <svg viewBox="0 0 50 50" class="loading-svg" :class="buttonSize">
         <circle cx="25" cy="25" r="20" fill="none" class="path"></circle>
       </svg>
     </div>
-    <slot v-else></slot>
+    <div class="b-button-slot" :class="isLoading?'b-button-slot-loading':''" @click="VueClick">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 import Lib from "../../../wrenches/Lib"
+
+const ButtonSize = {
+  small: 'b-button-small', // 24
+  middle: 'b-button-middle', // 32
+  default: 'b-button-default', // 40
+  large: 'b-button-large', // 48
+}
+
+const ButtonShape = {
+  default: '',
+  circle: 'b-button-shape-circle',
+  round: 'b-button-shape-round'
+}
 
 export default {
   name: 'BButton',
@@ -28,12 +43,30 @@ export default {
       type: String,
       default: '',
     },
-    handle: Function
+    handle: Function,
+    size: String,
+    block: Boolean,
+    shape: [String, Number]
   },
   computed: {
     isLoading() {
       return this.loading || this.debounceRunning || this.handleLoading
     },
+    buttonSize() {
+      return ButtonSize[this.size] || ButtonSize.default
+    },
+    buttonDisplay() {
+      return this.block ? 'b-button-block' : ''
+    },
+    buttonRadius() {
+      if (Lib.isNumber(this.shape)) {
+        return {'border-radius': this.shape + 'px'}
+      }
+      return {}
+    },
+    buttonShape() {
+      return ButtonShape[this.shape] || ButtonShape.default
+    }
   },
   data() {
     return {
@@ -51,6 +84,11 @@ export default {
      * @constructor
      */
     VueClick(event) {
+      // Loading
+      if (this.isLoading) {
+        return
+      }
+      // Debounce
       this.debounce(() => {
         // 存在路由地址跳转
         if (this.jump) {
@@ -99,12 +137,41 @@ export default {
 </script>
 
 <style lang="less">
+// animate
+@keyframes loading-dash {
+  0% {
+    stroke-dasharray: 1, 125;
+    stroke-dashoffset: 0;
+  }
+
+  50% {
+    stroke-dasharray: 95, 31;
+    stroke-dashoffset: -31px;
+  }
+
+  to {
+    stroke-dasharray: 6, 120;
+    stroke-dashoffset: -120px;
+  }
+}
+
+@keyframes loading-rotate {
+  to {
+    transform: rotate(1turn);
+  }
+}
+
 .b-button {
+  border: none;
+  outline: none;
+  -webkit-appearance: none;
+  // 去除button默认样式
   user-select: none;
   position: relative;
-  width: 200px;
-  height: 60px;
-  line-height: 60px;
+  display: inline-block;
+  //width: 200px;
+  //height: 60px;
+  //line-height: 60px;
   text-align: center;
   background: #0056FF;
   font-size: 18px;
@@ -112,6 +179,77 @@ export default {
   color: #FFFFFF;
   border-radius: 4px;
   cursor: pointer;
+  white-space: nowrap;
+  transition: all .4s;
+
+  &.b-button-block {
+    display: block;
+    width: 100%;
+  }
+
+  &.b-button-shape-round {
+    border-radius: 1000px;
+  }
+
+  & .b-button-slot {
+    display: inline-block;
+    width: 100%;
+    box-sizing: border-box;
+    transition: opacity .4s;
+
+
+    &.b-button-slot-loading {
+      opacity: 0;
+    }
+  }
+
+  &.b-button-small {
+    //margin: 2px 4px;
+    min-width: 40px;
+    line-height: 24px;
+    height: 24px;
+    font-size: 12px;
+
+    .b-button-slot {
+      padding: 0 8px;
+    }
+  }
+
+  &.b-button-middle {
+    //margin: 2px 4px;
+    min-width: 68px;
+    line-height: 32px;
+    height: 32px;
+    font-size: 12px;
+
+    .b-button-slot {
+      padding: 0 10px;
+    }
+  }
+
+  &.b-button-default {
+    //margin: 2px 4px;
+    min-width: 80px;
+    line-height: 40px;
+    height: 40px;
+    font-size: 14px;
+
+    .b-button-slot {
+      padding: 0 12px;
+    }
+  }
+
+  &.b-button-large {
+    //margin: 2px 4px;
+    min-width: 124px;
+    line-height: 48px;
+    height: 48px;
+    font-size: 18px;
+
+    .b-button-slot {
+      padding: 0 14px;
+    }
+  }
 
   &:hover {
     background: #004DE5;
@@ -128,68 +266,65 @@ export default {
     right: 0;
     bottom: 0;
     left: 0;
-
-    .loading-css {
-      box-sizing: border-box;
-      width: 50px;
-      height: 50px;
-      display: inline-block;
-      border: 5px solid #f3f3f3;
-      border-top: 5px solid red;
-      border-radius: 50%;
-      /* 动画旋转效果 */
-      animation: rotate-360 1s infinite linear;
-      display: none;
-    }
-
-    @keyframes rotate-360 {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
-    }
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     .loading-svg {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      margin: -15px 0 0 -15px;
-      width: 30px;
-      height: 30px;
       animation: loading-rotate 1.5s infinite ease-in-out;
-    }
 
-    @keyframes loading-rotate {
-      to {
-        transform: rotate(1turn);
+      & > .path {
+        stroke: rgba(255, 255, 255, 0.6);
+        stroke-width: 5;
+        animation: loading-dash 1.5s infinite ease-in-out;
+      }
+
+      &.b-button-small {
+        width: 16px;
+        height: 16px;
+      }
+
+      &.b-button-middle {
+        width: 20px;
+        height: 20px;
+      }
+
+      &.b-button-default {
+        width: 24px;
+        height: 24px;
+      }
+
+      &.b-button-large {
+        width: 30px;
+        height: 30px;
+
+        //& > .path {
+        //  stroke-width: 4;
+        //}
       }
     }
 
-    .loading-svg > .path {
-      stroke: rgba(255, 255, 255, 0.6);
-      stroke-width: 5;
-      animation: loading-dash 1.5s infinite ease-in-out;
-    }
+    //.loading-css {
+    //  box-sizing: border-box;
+    //  width: 50px;
+    //  height: 50px;
+    //  display: inline-block;
+    //  border: 5px solid #f3f3f3;
+    //  border-top: 5px solid red;
+    //  border-radius: 50%;
+    //  /* 动画旋转效果 */
+    //  animation: rotate-360 1s infinite linear;
+    //  display: none;
+    //}
 
-    @keyframes loading-dash {
-      0% {
-        stroke-dasharray: 1, 125;
-        stroke-dashoffset: 0;
-      }
-
-      50% {
-        stroke-dasharray: 95, 31;
-        stroke-dashoffset: -31px;
-      }
-
-      to {
-        stroke-dasharray: 6, 120;
-        stroke-dashoffset: -120px;
-      }
-    }
-
+    //@keyframes rotate-360 {
+    //  0% {
+    //    transform: rotate(0deg);
+    //  }
+    //  100% {
+    //    transform: rotate(360deg);
+    //  }
+    //}
   }
 }
 </style>
--- test/App.vue
+++ test/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
     {{ msg }}
-    <b-button @click="pro" :handle="sync1" :delay="1000">按钮测试</b-button>
+    <i @click="change">change</i>
+    <hr>
+    <b-button @click="pro" :handle="handle" :delay="1000" size="small" shape="round">小按钮小按钮小按钮小按钮小按钮</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="1000" size="middle" shape="round">中等测试</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="1000" shape="round">默认测试</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="1000" size="large" shape="round">大按钮</b-button>
+    <hr>
+    <b-button @click="pro" :handle="sync1" :delay="3000" size="small">Small</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="3000" size="middle">Middle</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="3000">Default</b-button>
+    <b-button @click="pro" :handle="sync1" :delay="3000" size="large" :shape="10">Large</b-button>
+    <hr>
+    <b-button :size="bSize" block>立即登录</b-button>
   </div>
 </template>
 
@@ -12,8 +24,9 @@ export default {
   name: 'App',
   data() {
     return {
-      msg: '1234',
-      loading: false
+      msg: '按钮测试',
+      loading: false,
+      bSize: 'small'
     }
   },
   components: {
@@ -26,6 +39,10 @@ export default {
     console.log(p)
   },
   methods: {
+    change() {
+      this.bSize = this.bSize === 'small' ? 'large' : 'small'
+    }
+    ,
     lo() {
       // setTimeout(() => {
       //   this.loading = true
@@ -34,8 +51,8 @@ export default {
     handle() {
       const p = new Promise((resolve => {
         setTimeout(() => {
-          resolve(6)
-        }, 5000)
+          resolve(1000)
+        }, 3000)
       }))
       p.finally((e) => {
         console.log('XXXXXXXXX')
@@ -46,7 +63,7 @@ export default {
       return p
     },
     async pro(e) {
-      console.log('**********', await e)
+      console.log('**********打印', await e)
     },
     sync1() {
       console.log(1)
